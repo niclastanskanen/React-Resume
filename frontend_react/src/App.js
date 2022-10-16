f3e12f88652a2ffb9bdbb29c4ccb11a3eabf0ced
niclastanskanen <niclastanskanen@hotmail.com>
2022-10-16 00:59:07 +0000

    Add

---
+++ frontend_react/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
