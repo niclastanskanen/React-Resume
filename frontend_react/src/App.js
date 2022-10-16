3f037638f79f8d7e53314c058d2e73f1ee73f372
niclastanskanen <niclastanskanen@hotmail.com>
2022-10-16 18:28:08 +0000

    Add

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <Header />
       <About />
       <Work />
